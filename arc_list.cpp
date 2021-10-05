967170187e485135320d085c7561294d357b40d9
Edwin <edgutierrez01@hotmail.com>
2021-10-05 10:10:09 -0600

    Actualizacion person y arc_list

--- arc_list.cpp
+++ arc_list.cpp
@@ -1,5 +1,7 @@
 #include "arc_list.h"
+
 using namespace std;
+
 arc_list::arc_list()
 {
     item=nullptr;
@@ -12,6 +14,21 @@ arc_list::~arc_list()
     //dtor
 }
 
+
+void set_item(std::shared_ptr<person> nuevo_item){
+    item=nuevo_item;
+}
+void set_next(std::shared_ptr<arc_node> siguiente){
+    next=siguiente;
+}
+std::shared_ptr<arc_node> get_next(){
+    return next;
+}
+std::shared_ptr<person> get_item(){
+    return item;
+}
+
+
 /**
 
 REQ: Requiere una persona.
@@ -21,6 +38,14 @@ EFE: Devuelve el puntero si se encuentra a la persona solicitada.
 */
 shared_ptr<arc_list::arc_node> arc_list::find(const person& target) const
 {
-
+    bool encontrado=false;
+    shared_ptr<arc_list::arc_node> iterador=next;
+    while(iterador!=nullptr&&!encontrado){
+        if(iterador->m_nickname=target){
+                encontrado=true;
+                return iterador;
+        }else{
+            iterador=iterador->get_next();
+        }
+    }
 }
-
--- person.cpp
+++ person.cpp
@@ -3,38 +3,37 @@
 using namespace std;
 person::person()
 {
-    m_nickname="null"; 
-    m_fullname="null"; 
-    m_age=0; 
-    m_sex=false; 
-    m_hobbies[]={"null","null","null","null","null","null","null","null","null","null"}; 
+    m_nickname="null";
+    m_fullname="null";
+    m_age=0;
+    m_sex=false;
+
 }
 
 person::~person()
 {
-    delete this.person;
 }
 
 void set_m_nickname(string apodo)//!< Member variable "m_nickname"
 {
     m_nickname=apodo;
-} 
+}
 void set_m_fullname(string nombre)//!< Member variable "m_fullname"
 {
     m_fullname=nombre;
-} 
+}
 void set_m_age(unsigned int edad)//!< Member variable "m_age"
 {
     m_age=edad;
-} 
+}
 void set_m_sex(bool sexo)//!< Member variable "m_sex"
 {
     m_sex=sexo;
-} 
+}
 void set_m_hobbies(string hobby[])//!< Member variable "m_hobbies"
 {
-    m_hobbies[]=hobby;
-} 
+    m_hobbies=hobby;
+}
 void set_m_hobbies(string hobby)//!< Member variable "m_hobbies"
 {
     int iter=0;
@@ -46,26 +45,25 @@ void set_m_hobbies(string hobby)//!< Member variable "m_hobbies"
         }
         iter++;
     }
-} 
+}
 
 string get_m_nickname()//!< Member variable "m_nickname"
 {
     return m_nickname;
-} 
+}
 string get_m_fullname()//!< Member variable "m_fullname"
 {
     return m_fullname;
-} 
+}
 unsigned int get_m_age()//!< Member variable "m_age"
 {
     return m_age;
-} 
+}
 bool get_m_sex()//!< Member variable "m_sex"
 {
     return m_sex;
-} 
+}
 string get_m_hobbies()//!< Member variable "m_hobbies"
 {
     return m_hobbies[];
-} 
-
+}
